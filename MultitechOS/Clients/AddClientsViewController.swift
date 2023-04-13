69f6717b16209fbe184e8aab8aa6e26f8226cc13
Felipe Domingos <felipedomingos@MacBook-Pro-de-Felipe.local>
2023-04-13 11:22:14 -0300

    Passado todas as regras de negocios da AddclientsViewController para sua respectiva ViewModel

--- MultitechOS/Clients/AddClientsViewController.swift
+++ MultitechOS/Clients/AddClientsViewController.swift
@@ -10,30 +10,30 @@ import UIKit
 class AddClientsViewController: UIViewController, UITextFieldDelegate {
     
     weak var coordinator: MainCoordinator?
-    var custumerBaseViewScreen = CustumerBaseViewScreen()
+    var custumerBaseViewScreen: CustumerBaseViewScreen?
     var viewModel = RegisterViewModel()
     
     override func loadView() {
-        self.view = self.custumerBaseViewScreen
+        custumerBaseViewScreen = CustumerBaseViewScreen()
+        view = custumerBaseViewScreen
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        self.custumerBaseViewScreen.cepTextField.delegate = self
+        self.custumerBaseViewScreen?.cepTextField.delegate = self
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        viewModel.validateCepField(textField: custumerBaseViewScreen.cepTextField, cep: custumerBaseViewScreen.cepTextField.text ?? "") { register in
-            self.addData(register: register)
-            print(register)
+        viewModel.validateCepField(textField: custumerBaseViewScreen?.cepTextField ?? textField, cep: custumerBaseViewScreen?.cepTextField.text ?? "") { register in
+            self.initViewModel(data: register)
         }
     }
     
-    func addData(register: Register) {
-        custumerBaseViewScreen.nameStreetTextField.text = register.logradouro
-        custumerBaseViewScreen.districtTextField.text = register.bairro
-        custumerBaseViewScreen.cityTextField.text = register.localidade
-        custumerBaseViewScreen.StateTextField.text = register.uf
+    func initViewModel(data: Register) {
+        if let screen = self.custumerBaseViewScreen {
+            viewModel = RegisterViewModel(custumerBaseViewScreen: screen)
+        }
+        viewModel.addData(register: data)
     }
 }
